40f9df38c1e3b2d4b97a44e5583b589922623b52
i-kyle <i-kyle@mercari.com>
2018-08-03 11:18:11 -0700

    some improvement around camera

--- app/src/main/java/com/example/kylerfcristin/snapface/Singleton/CameraModule.java
+++ app/src/main/java/com/example/kylerfcristin/snapface/Singleton/CameraModule.java
@@ -10,4 +10,18 @@ public class CameraModule {
             mCamera = Camera.open(0);
         } return mCamera;
     }
+
+    public static void turnOnFlash() {
+        Camera.Parameters myCameraParameters = mCamera.getParameters();
+        String stringFlashMode;
+        stringFlashMode = myCameraParameters.getFlashMode();
+        if (stringFlashMode.equals("torch"))
+            myCameraParameters.setFlashMode("on");
+        else
+            myCameraParameters.setFlashMode("torch");
+        mCamera.setParameters(myCameraParameters);
+        if (stringFlashMode.equals("on")) {
+            myCameraParameters.setFlashMode("off");
+        }
+    }
 }
